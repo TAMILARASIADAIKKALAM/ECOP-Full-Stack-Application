1eb646957e77de4e1d04616611b5f826631ce3e3
TAMILARASI <111672815+TAMILARASIADAIKKALAM@users.noreply.github.com>
2023-08-24 07:04:32 +0530

    Add files via upload

---
+++ Backend-main/Backend-main/E-COP/E-COP/src/test/java/com/example/ECOP/ECopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ECOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
